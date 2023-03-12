2c0a0715af66dd9a5b10769b5a45190fd684abaf
HasegawaTaizou <caiocoghi@gmail.com>
2023-03-12 13:07:38 -0300

    fixing first access part two

---
+++ js/show-confirm-password.js
@@ -0,0 +1,22 @@
+// "use strict";
+
+// const passwordConfirmIconEye = document.querySelector(".confirm");
+// const passwordConfirmIconEyeSlash = document.querySelector(".confirm");
+
+// const handleConfirmClick = function () {
+//   const confirmPasswordInput = document.querySelector(
+//     ".confirm-password__input"
+//   );
+//   if (confirmPasswordInput.getAttribute("type") == "password") {
+//     confirmPasswordInput.setAttribute("type", "text");
+//     passwordConfirmIconEyeSlash.classList.add("block");
+//     passwordConfirmIconEye.classList.add("none");
+//   } else {
+//     confirmPasswordInput.setAttribute("type", "password");
+//     passwordConfirmIconEyeSlash.classList.remove("block");
+//     passwordConfirmIconEye.classList.remove("none");
+//   }
+// };
+
+// passwordConfirmIconEye.addEventListener("click", handleConfirmClick);
+// passwordConfirmIconEyeSlash.addEventListener("click", handleConfirmClick);
